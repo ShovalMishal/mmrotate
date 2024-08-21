365b8a6a206bcfe4758ab45d0d4d73fa24da0442
Shoval <shoval@brdcm04-212-pcl.eng.tau.ac.il>
2024-08-21 18:41:32 +0300

    new config

--- mmrotate/datasets/dota.py
+++ mmrotate/datasets/dota.py
@@ -334,4 +334,19 @@ class DOTAv2DatasetOOD7(DOTADataset):
         'palette': [(165, 42, 42)]
     }
 
+@DATASETS.register_module()
+class DOTAv2DatasetOOD8(DOTADataset):
+    """DOTA-v2.0 dataset for detection.
+
+    Note: ``ann_file`` in DOTAv2Dataset is different from the BaseDataset.
+    In BaseDataset, it is the path of an annotation file. In DOTAv2Dataset,
+    it is the path of a folder containing XML files.
+    """
+
+    METAINFO = {
+        'classes':
+        ('bridge',),
+        # palette is a list of color tuples, which is used for visualization.
+        'palette': [(165, 42, 42)]
+    }
 
